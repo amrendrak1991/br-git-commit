d9aa6d5deef1a734f21af45cd8eafa8705accb23
Amrendra <amrendra.k1991@gmail.com>
2021-03-06 19:49:18 +0530

    tried jest configuration wip

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders home page with about project test', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Simple app/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+//Jest configuration is giving weired error due to antd lib, need to spend more time for proper configuration
